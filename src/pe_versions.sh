5990d09a959732e4aadb11ce2863470fa5dab16f
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-05-10 21:18:35 -0700

    moved pe_version task to standalone script

--- src/pe_versions.sh
+++ src/pe_versions.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-year=$PT_year
+if [ "$#" -ne 1 ]; then
+    echo "Error: Illegal number of parameters"
+    echo "Usage: pe_versions.sh <2016|2017|2018|2019>"
+    exit
+fi
+
+year=$1
+
+if [ $1 -ne 2016 ] && [ $1 -ne 2017 ] && [ $1 -ne 2018 ] && [ $1 -ne 2019 ]; then
+   echo "Error: Valid PE version years are 2016-2019"
+   echo "Usage: pe_versions.sh <2016|2017|2018|2019>"
+   exit 
+fi
 
 # generate a temp file with a random part in the name
 version_file="/tmp/pe-versions-`date +'%s%m%y'`.txt"
@@ -11,7 +23,7 @@ curl -s http://versions.puppet.com.s3-website-us-west-2.amazonaws.com/ > $versio
 echo -n "Puppet Enterprise versions released in Year $year :"
 # grep all the versions in that year.
 values=$(grep $year $version_file)
-if [ -z $values ]; then
+if [ -z "$values" ]; then
     echo " Zero matches"
 else
     echo ""
