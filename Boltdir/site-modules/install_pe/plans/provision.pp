6e4faf6bd3358600671c17e92fae5e98d1414f1d
Rajesh Radhakrishnan <enthoughts@gmail.com>
2020-05-25 20:53:00 -0700

    added hostname config

--- Boltdir/site-modules/install_pe/plans/provision.pp
+++ Boltdir/site-modules/install_pe/plans/provision.pp
@@ -3,16 +3,27 @@
 plan install_pe::provision (
   String $base_os_vmx_file,
   String $pe_version,
+  Optional[String]  $r10k_remote = undef,
+  Optional[String]  $r10k_private_key_file = undef,
 )
 {
   $localhost = get_targets('localhost')
 
-  notice("#{Target}")
+  # Use vmrun to create a new VM for hosting PE
+  $pe_changed_ver_string=regsubst($pe_version,'\.','_','G')
   $pe_host_ip = run_task('install_pe::clone_create_vm',$localhost, 
-                      base_os_vmx_file => $base_os_vmx_file, pe_version => $pe_version).first['ip']
+                          base_os_vmx_file => $base_os_vmx_file, pe_version => $pe_changed_ver_string).first['ip']
+  #out::message("IP = $pe_host_ip")
+
+  # Turn PE host IP into Bolt targets, and add to inventory
+  $targets = Target.new("${$pe_host_ip}").add_to_group('vmware_ws')
+
+  # set the Host name of PE-master and reboot  
+  run_command("hostnamectl set-hostname pe${pe_changed_ver_string}",$targets,_run_as => 'root','_catch_errors'=>true)
+  run_command("sed -i 's/ubuntu18/pe${pe_changed_ver_string}/' /etc/hosts",$targets,_run_as => 'root','_catch_errors'=>true)
+  run_task("reboot",$targets) # 
+
+  # install PE on the PE-host
 
 
-  out::message("IP = $pe_host_ip")
-
-  #run_task($pe_version, $pe_host)
 }
